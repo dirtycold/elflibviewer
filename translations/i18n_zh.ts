7a0f418a7082a67367252fa64ea77a7e5f868d7e
ZH <zhhhhz@zoho.com>
2013-09-19 18:40:57 +0800

    added i18n_zh.ts
    the plain editable translation

---
+++ translations/i18n_zh.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../elflibviewer.ui" line="14"/>
+        <source>ELF Library Viewer</source>
+        <translation>ELF 依赖查看器</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="27"/>
+        <source>Clear the selected library name</source>
+        <translation>清除所选依赖库名称</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="30"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="44"/>
+        <source>Type a library name in here to highlight libraries that use it</source>
+        <translation>在此输入依赖库名称以查看其使用情况</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="47"/>
+        <source>Search...</source>
+        <translation>搜索…</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="79"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="87"/>
+        <source>&amp;Help</source>
+        <translation>帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="101"/>
+        <source>&amp;Open...</source>
+        <translation>打开(&amp;O)…</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="104"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="113"/>
+        <source>&amp;Quit</source>
+        <translation>退出(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="116"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../elflibviewer.ui" line="124"/>
+        <source>&amp;About...</source>
+        <translation>关于(&amp;A)…</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowImpl</name>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="62"/>
+        <source>Shared Object</source>
+        <translation>共享对象</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="62"/>
+        <source>Resolved Path</source>
+        <translation>解析路径</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="87"/>
+        <source>Open executable or library</source>
+        <translation>打开可执行文件或共享库</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="107"/>
+        <source>not found</source>
+        <translation>未找到</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="146"/>
+        <source>%1 dependencies with total size %2 MB.</source>
+        <translation>共 %1 个依赖共占用 %2 MB 空间。</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="152"/>
+        <source>%1 dependencies with total size %2 KB.</source>
+        <translation>共 %1 个依赖共占用 %2 KB 空间。</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="154"/>
+        <source>%1 dependencies with total size %2 bytes.</source>
+        <translation>共 %1 个依赖共占用 %2 字节 空间。</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="159"/>
+        <source>About elflibviewer</source>
+        <translation>关于 ELF 依赖查看器</translation>
+    </message>
+    <message>
+        <location filename="../MainWindowImpl.cpp" line="160"/>
+        <source>elflibviewer is a program to display the required library dependencies of a program or shared library (in ELF format).  It requires the readelf tool.
+
+Copyright Â© 2007, 2009 Michael Pyne.  This program may be distributed under the terms of the GNU GPL v2 (or any later version).
+
+Icons are from the KDE 4 Oxygen icon set, distributed under the LGPL, version 2.
+
+Version: %1</source>
+        <translation>ELF 依赖查看器是一个用来显示程序或共享库 (ELF 格式) 所需依赖库的程序。运行需要 readelf 程序。
+
+Copyright Â© 2007, 2009 Michael Pyne. 此程序遵循 GNU GPL 第2版 (或任何后续版本)。
+
+程序中图标取自 KDE 4 的 Oxygen 图标集，其遵循 LGPL 第2版。
+
+程序版本: %1</translation>
+    </message>
+</context>
+</TS>
